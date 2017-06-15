db53f123c6a1c4a1b8c7c604c42e6ebb8d8a6a10
jwsmith <joshua.wyatt.smith@cern.ch>
2017-06-15 19:06:34 +0200

    add the sample lists

---
+++ samples/w.sh
@@ -0,0 +1,6 @@
+./ttgammaFlattener 301XXX.enugamma.p2952.v007.001.root
+./ttgammaFlattener 301XXX.munugamma.p2952.v007.001.root
+./ttgammaFlattener 301XXX.taunugamma.p2952.v007.001.root
+./ttgammaFlattener 3641XX.Wenu.p2952.v007.001.root
+./ttgammaFlattener 3641XX.Wmunu.p2952.v007.001.root
+./ttgammaFlattener 3641XX.Wtaunu.p2952.v007.001.root
---
+++ samples/z.sh
@@ -0,0 +1,6 @@
+./ttgammaFlattener 301XXX.eegamma.p2952.v007.001.root
+./ttgammaFlattener 301XXX.mumugamma.p2952.v007.001.root
+./ttgammaFlattener 301XXX.tautaugamma.p2952.v007.001.root
+./ttgammaFlattener 3641XX.Zee.p2952.v007.001.root
+./ttgammaFlattener 3641XX.Zmumu.p2952.v007.001.root
+./ttgammaFlattener 3641XX.Ztautau.p2952.v007.001.root
